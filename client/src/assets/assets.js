0eb3f51ea4a30de82563cc63f12aa626e18d9d3c
thusykanna <thusy0816@gmail.com>
2025-07-26 00:59:57 +0530

    Add video banner asset and update Header component to display it

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -1,5 +1,7 @@
 import logo from './logo.png';
+import video_banner from './home-page-banner.mp4'
 
 export const assets = {
-    logo
+    logo,
+    video_banner
 }
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
 const Header = () => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 gap-12 items-center mb-16">
 
         {/* Left side: video banner */}
-        
+        <div className="order-2 md:order-1 flex justify-center">
+
+            <div className="shadow-[0_25px_50px_-12px_rgba(0,0,0,0.15)] rounded-3xl overflow-hidden">
+
+                <video src={assets.video_banner} autoPlay loop muted className='w-full max-w-[400px] h-auto object-cover'/>
+            </div>
+
+
+        </div>
 
 
         {/* Right side: text component */}
