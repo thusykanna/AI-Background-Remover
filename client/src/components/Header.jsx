a52dbf5e6045af76420470e90a8e14a034bd5a4e
thusykanna <thusy0816@gmail.com>
2025-07-26 01:32:54 +0530

    Refactor Header component layout and add upload functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,25 +2,43 @@ import React from 'react'
 import { assets } from '../assets/assets'
 
 const Header = () => {
-  return (
-    <div className="grid grid-cols-1 md:grid-cols-2 gap-12 items-center mb-16">
+    return (
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-12 items-center mb-16">
 
-        {/* Left side: video banner */}
-        <div className="order-2 md:order-1 flex justify-center">
+            {/* Left side: video banner */}
+            <div className="order-2 md:order-1 flex justify-center">
 
-            <div className="shadow-[0_25px_50px_-12px_rgba(0,0,0,0.15)] rounded-3xl overflow-hidden">
+                <div className="shadow-[0_25px_50px_-12px_rgba(0,0,0,0.15)] rounded-3xl overflow-hidden">
+                    <video src={assets.video_banner} autoPlay loop muted className='w-full max-w-[400px] h-auto object-cover' />
+                </div>
 
-                <video src={assets.video_banner} autoPlay loop muted className='w-full max-w-[400px] h-auto object-cover'/>
             </div>
 
 
+            {/* Right side: text component */}
+            <div className="order-1 md:order-2">
+                <h1 className="text-4xl md:text-5xl font-bold text-gray-900 mb-6 leading-tight">
+                    The fastest <span className="text-indigo-700">
+                        background remover.
+                    </span>
+                </h1>
+                <p className="text-gray-600 mb-8 text-lg leading-relaxed">
+                    Transform your photos with our background remover app! Highlight your 
+                    subject and create a transparent background, so you can place it in a 
+                    variety of new designs and destinations. Try it now and immerse your 
+                    subject in a completely different environment!
+                </p>
+
+                <div>
+                    <input type="file" accept='"image/*' id='upload1' hidden />
+                    <label htmlFor="" className="bg-black text-white font-medium px-8 py-4 rounded-full hover:opacity-90 transition-transform hover:scalable-105 text-lg">
+                       Upload your image
+                    </label>
+                </div>
+            </div>
+
         </div>
-
-
-        {/* Right side: text component */}
-
-    </div>
-  )
+    )
 }
 
 export default Header
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import Header from "../components/Header";
 
 const Home = () => {
     return (
-        <div>
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-16 font-['Outfit']">
             {/* Hero section */}
             <Header />
 
