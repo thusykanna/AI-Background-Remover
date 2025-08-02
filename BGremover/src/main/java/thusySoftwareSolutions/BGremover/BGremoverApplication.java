5773bda51424292a494f19e40370f104f21a4998
thusykanna <thusy0816@gmail.com>
2025-08-03 00:32:39 +0530

    Spring Initislizer

---
+++ BGremover/src/main/java/thusySoftwareSolutions/BGremover/BGremoverApplication.java
@@ -0,0 +1,13 @@
+package thusySoftwareSolutions.BGremover;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BGremoverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BGremoverApplication.class, args);
+	}
+
+}
---
+++ BGremover/src/test/java/thusySoftwareSolutions/BGremover/BGremoverApplicationTests.java
@@ -0,0 +1,13 @@
+package thusySoftwareSolutions.BGremover;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BGremoverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
